d06e2b4c8904e2b7bcd60b086c0967c74c08d675
821453366 <821453366@qq.com>
2017-08-10 11:02:17 +0800

    打印歌词

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+var main = require("./lib/main.js");
+var result = main(5);
+console.log(result);
+
+var result = main(1);
+console.log(result);
+
+var result = main(99);
+console.log(result);
